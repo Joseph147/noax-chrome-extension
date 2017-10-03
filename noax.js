4223862aa8cddc8f9dd6d692e2c0c54eedcd6049
Kaioru <keifuurawrs@gmail.com>
2017-10-03 14:06:40 +0800

    Update endpoints

--- noax.js
+++ noax.js
@@ -1,7 +1,7 @@
 var url = location.href;
 var protocol = url.split("/")[0];
 
-$.getJSON(protocol + '//noax.kaioru.co/api/count/' + encodeURIComponent(url),
+$.getJSON(protocol + '//noax.dev/api/count/' + encodeURIComponent(url),
   function(data) {
     if (data.block || data.flagged) {
       alert('This page has been reported ' + data.count + ' times.')
